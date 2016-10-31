faaac32ec08541706db8fbc0fbb51a1658bc7aa2
Shabith Ishan <shabith.ishan@gmail.com>
2016-10-31 23:12:51 +0530

    initial commit

---
+++ spec/g-webfonts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GWebfontsView from '../lib/g-webfonts-view';
+
+describe('GWebfontsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
